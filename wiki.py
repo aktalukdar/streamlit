6f29a35946375fef2a056ef2ae67f8c36349c739
Afzal Karim Talukdar <54910389+aktalukdar@users.noreply.github.com>
2021-04-12 10:30:23 +0530

    Update wiki.py

--- wiki.py
+++ wiki.py
@@ -1,6 +1,13 @@
 import wikipedia
 import random
 def getData(a):
-    result = wikipedia.search(a)
-    page = wikipedia.page(result[0], preload= True)
-    return page.content
+    if(a==""):
+        return "Please enter any input."
+    try:
+        wiki_topic = wikipedia.search(a)
+        page = wikipedia.page(title=wiki_topic,auto_suggest=True,redirect=True, preload=False)
+        return page.content
+    except wikipedia.exceptions.PageError as error:
+        return "No Page could be found with the topic you entered!"
+    except wikipedia.exceptions.DisambiguationError as e:
+        return "Please be more specific with your search query as there are a couple of other options meaning the same."
